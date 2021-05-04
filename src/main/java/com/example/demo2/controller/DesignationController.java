ec98c85cafbf0b86eebcc3b0d2249c028578bfa2
sreekanthreddypb <78473085+sreekanthreddypb@users.noreply.github.com>
2021-05-04 10:57:20 +0000

    Update DesignationController.java

--- src/main/java/com/example/demo2/controller/DesignationController.java
+++ src/main/java/com/example/demo2/controller/DesignationController.java
@@ -30,6 +30,7 @@ public class DesignationController {
 	@GetMapping("/getAllDEsignation")
 	public ResponseEntity<List<Designation>> getDesignation(){
 		return new ResponseEntity<List<Designation>>(DesignationService.getDesignation(), HttpStatus.OK);
+		System.out.println("No data Available");
 	}
 		
 	
