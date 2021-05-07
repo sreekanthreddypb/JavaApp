f0f75356f74eb6cbb7a3fecfb54cc2299b0e3bb8
sreekanthreddypb <78473085+sreekanthreddypb@users.noreply.github.com>
2021-05-07 11:37:12 +0000

    Create ServletInitializer.java

---
+++ src/main/java/com/example/demo2/ServletInitializer.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.example.demo2.Demo2Application;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Demo2Application.class);
+	}
+
+}
